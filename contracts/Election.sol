a94a51e3aeeb1c1a01d175b024af1bf64db863e8
James Nolan <vip17jamesnolan@gmail.com>
2022-04-12 17:32:47 +0800

    feat(dep): add candidate info

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -4,16 +4,24 @@ pragma solidity >=0.4.22 <0.9.0;
 contract Election {
   address admin;
   string electionName;
-  string electionId;
-  uint256 startTime;
-  uint256 endTime;
-  string[] options;
-  uint256 participantNum;
+  uint electionId;
+  uint startTime;
+  uint endTime;
+  // string[] options;
+  uint participantNum;
   bool allowMultipleOption;
   bool stopOnTime;
 
   constructor() public {
     admin = msg.sender;
+    electionName = "";
+    electionId = 0;
+    startTime = 0;
+    endTime = 0;
+    // options = [];
+    participantNum = 0;
+    allowMultipleOption = false;
+    stopOnTime = true;
   }
 
   modifier onlyAdmin{
@@ -25,10 +33,32 @@ contract Election {
     return admin;
   }
 
-  function handOverAdmin(address _newAdmin) public
+  function setAdmin(address _newAdmin) public
   onlyAdmin
   {
     require(_newAdmin == address(0x0));
     admin = _newAdmin;
+  }
+
+  function getElectionName() public view returns (string memory){
+    return electionName;
+  }
+
+  function setElectionName(string memory newElectionName) public onlyAdmin{
+    electionName = newElectionName;
+  }
+
+  function getElectionId() public view returns (uint){
+    return electionId;
+  }
+
+  function setElectionId(uint newElectionId) public onlyAdmin{
+    electionId = newElectionId;
+  }
+
+  struct Candidate{
+    uint id;
+    uint token;
+
   }
 }
