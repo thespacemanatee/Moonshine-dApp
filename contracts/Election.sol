f4a61e70218fdd7c25281e42eb2291284bf36618
James Nolan <vip17jamesnolan@gmail.com>
2022-04-12 16:20:47 +0800

    upload file

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,6 +2,33 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Election {
+  address admin;
+  string electionName;
+  string electionId;
+  uint256 startTime;
+  uint256 endTime;
+  string[] options;
+  uint256 participantNum;
+  bool allowMultipleOption;
+  bool stopOnTime;
+
   constructor() public {
+    admin = msg.sender;
+  }
+
+  modifier onlyAdmin{
+    if (msg.sender != admin) throw;
+    _;
+  }
+
+  function getAdmin() public view returns (address){
+    return admin;
+  }
+
+  function handOverAdmin(address _newAdmin)
+  onlyAdmin
+  {
+    if(_newAdmin == 0x0) throw;
+    admin = _newAdmin;
   }
 }
