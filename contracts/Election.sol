00a670701866c00f5a7a0fb4f8a9702fee7e04c1
James Nolan <vip17jamesnolan@gmail.com>
2022-04-16 01:41:54 +0800

    feat(constract): finish the contract, can check

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,66 +2,113 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Election {
-    address admin;
-    uint256 startTime;
-    uint256 endTime;
-    // string[] options;
-    uint256 candidateCount;
-    bool allowMultipleOption;
-    bool stopOnTime;
-    bool inProgress;
-
-    constructor() {
-        admin = msg.sender;
-        startTime = 0;
-        endTime = 0;
-        // options = [];
-        allowMultipleOption = false;
-        stopOnTime = true;
-        inProgress = false;
-    }
-
-    // Modeling a Election Details
-    struct ElectionDetails {
+    struct ElectionInfo {
         string electionName;
-        string organizationName;
+        uint256 electionId; // Can set the creation time of election as Id.
+        uint256 startTime;  //-1 by default (Start right after a election is created)
+        uint256 endTime;    //-1 by default (Admin can manually end the election)
+        bool start;
+        bool end;
     }
-
-    ElectionDetails electionDetails;
-
     struct Candidate {
         uint256 candidateId;
         string name;
         string description;
         uint256 voteCount;
     }
-    
-    mapping(uint256 => Candidate) public candidateSet;
-
     struct Voter {
         address voterAddress;
         bool hasVoted;
+        bool isRegistered;
         bool isVerified;
     }
-    
-    address[] public registeredVoters; // Array of address to store address of voters
+    //Here are all the variables
+    address admin; //The creator of this election
+    ElectionInfo electionInfo;
+    mapping(uint256 => Candidate) public candidateSet;
     mapping(address => Voter) public voterSet;
+    constructor() public {
+        admin = msg.sender;
+        electionInfo = ("", block.timestamp, -1, -1, true, false);
+        // options = [];
+        participantCount = 0;
+        allowMultipleOption = false;
+        stopOnTime = true;
+        start = true;
+        end = false;
+    }
 
     modifier onlyAdmin() {
         require(msg.sender == admin);
         _;
     }
 
-    // Initialize and start the election
-    function initElection(
-        string memory _electionName,
-        string memory _organizationName
-    ) public onlyAdmin {
-        electionDetails = ElectionDetails({
-            electionName: _electionName,
-            organizationName: _organizationName
-        });
-        inProgress = true;
+    //Getters and Setters
+    function getAdmin() public view returns (address) {
+        return admin;
+    }
+
+    function setAdmin(address _newAdmin) public onlyAdmin {
+        require(_newAdmin == address(0x0));
+        admin = _newAdmin;
+    }
+
+    function getElectionName() public view returns (string memory) {
+        return electionName;
+    }
+
+    function setElectionName(string memory newElectionName) public onlyAdmin {
+        electionName = newElectionName;
+    }
+
+    function getElectionId() public view returns (uint256) {
+        return electionId;
+    }
+
+    function setElectionId(uint256 newElectionId) public onlyAdmin {
+        electionId = newElectionId;
+    }
+
+    function getStartTime() public view returns (uint256) {
+        return startTime;
+    }
+
+    function setStartTime(uint256 NewstartTime) public onlyAdmin {
+        startTime = NewstartTime;
+    }
+
+    function getEndTime() public view returns (uint256) {
+        return endTime;
+    }
+
+    function setEndTime(uint256 NewendTime) public onlyAdmin {
+        endTime = NewendTime;
+    }
+
+    function getParticipantCount() public view returns (uint256) {
+        return participantCount;
+    }
+
+    function setParticipantCount(uint256 NewparticipantNum) public onlyAdmin {
+        participantCount = NewparticipantNum;
+    }
+
+    function getAllowMultipleOption() public view returns (uint) {
+        return startTime;
+    }
+
+    function setAllowMultipleOption(bool NewAllowMultipleOption) public
+    onlyAdmin
+    {
+        allowMultipleOption = NewAllowMultipleOption;
+    }
+
+    function getStopOnTime() public view returns (bool) {
+        return stopOnTime;
+    }
+
+    function setStopOnTime(bool NewstopOnTime) public onlyAdmin {
+        stopOnTime = NewstopOnTime;
     }
 
     // Adding new candidates
@@ -78,21 +125,26 @@ contract Election {
         candidateSet[candidateCount] = newCandidate;
         candidateCount += 1;
     }
+
+    address[] public RegisteredVoters; // Array of address to store address of voters
     
-    // Register a voter
+    //register a voter
     function registerVoter() public  {
         Voter memory newVoter =
             Voter({
                 voterAddress: msg.sender,
                 hasVoted: false,
+                isRegistered: true,
                 isVerified: false
             });
-        registeredVoters.push(msg.sender);
         voterSet[msg.sender] = newVoter;
+        RegisteredVoters.push(msg.sender);
+        participantCount += 1;
     }
 
     // Verify a voter
-    function verifyVoter(address voterAddress) public onlyAdmin
+    function verifyVoter(address voterAddress) public 
+    onlyAdmin
     {
         voterSet[voterAddress].isVerified = true;
     }
@@ -100,63 +152,18 @@ contract Election {
     // Vote
     function vote(uint256 candidateId) public {
         require(voterSet[msg.sender].hasVoted == false);
+        require(voterSet[msg.sender].isRegistered == true);
         require(voterSet[msg.sender].isVerified == true);
-        require(inProgress == true);
+        require(start == true);
+        require(end == false);
         candidateSet[candidateId].voteCount += 1;
         voterSet[msg.sender].hasVoted = true;
     }
 
     // End election
     function endElection() public onlyAdmin {
-        require(inProgress == true);
-        inProgress = false;
+        end = true;
+        start = false;
     }
 
-    // Getters and Setters
-    function getAdmin() public view returns (address) {
-        return admin;
-    }
-
-    function setAdmin(address _newAdmin) public onlyAdmin {
-        require(_newAdmin == address(0x0));
-        admin = _newAdmin;
-    }
-    
-    function getStartTime() public view returns (uint256) {
-        return startTime;
-    }
-
-    function setStartTime(uint256 newStartTime) public onlyAdmin {
-        startTime = newStartTime;
-    }
-
-    function getEndTime() public view returns (uint256) {
-        return endTime;
-    }
-
-    function setEndTime(uint256 newEndTime) public onlyAdmin {
-        endTime = newEndTime;
-    }
-
-    function getParticipantCount() public view returns (uint256) {
-        return registeredVoters.length;
-    }
-
-    function getAllowMultipleOption() public view returns (uint) {
-        return startTime;
-    }
-
-    function setAllowMultipleOption(bool newAllowMultipleOption) public
-    onlyAdmin
-    {
-        allowMultipleOption = newAllowMultipleOption;
-    }
-
-    function getStopOnTime() public view returns (bool) {
-        return stopOnTime;
-    }
-
-    function setStopOnTime(bool newStopOnTime) public onlyAdmin {
-        stopOnTime = newStopOnTime;
-    }
 }
